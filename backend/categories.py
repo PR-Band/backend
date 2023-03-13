b3cf5fe97ec9865360edced43d0f5785982d7dc3
Andrey Rukin <rukin897@gmail.com>
2023-03-13 23:14:57 +0300

    Chore: Flake8 готов

--- backend/categories.py
+++ backend/categories.py
@@ -1,24 +1,19 @@
-from flask import Flask, jsonify, request, abort, Blueprint
 from http import HTTPStatus
 from uuid import uuid4
 
+from flask import Blueprint, abort, jsonify, request
+
 view = Blueprint('categories', __name__)
-
-
-
-
 categories = [
-    {"id": uuid4().hex,
-     "title": "Тренировки по волейболу"},
-    {"id": uuid4().hex,
-     "title": "Массаж"}
+    {'id': uuid4().hex,
+     'title': 'Тренировки по волейболу',
+     },
+    {'id': uuid4().hex,
+     'title': 'Массаж',
+     },
 ]
-
-# storage = {}
-# for category in categories:
-#     storage[category['id']] = category
-
 storage = {category['id']: category for category in categories}
+
 
 @view.get('/')
 def get_all_categories():
@@ -32,13 +27,12 @@ def get_category_by_id(uid):
     category = storage.get(uid)
     if category:
         return jsonify(category), 200
-    abort(404)
+    abort(HTTPStatus.NOT_FOUND)
 
 
 @view.post('/')
 def add_category():
     # получить тело запроса можно с помощью модуля request
-    # payload = request.json()
     category = request.json
     category['id'] = uuid4().hex
     storage[category['id']] = category
@@ -51,7 +45,7 @@ def update_category(uid):
     # должны вернуть измененный объект
     category = storage.get(uid)
     if uid not in storage:
-        abort(404)
+        abort(HTTPStatus.NOT_FOUND)
 
     payload = request.json
     category.update(payload)
@@ -62,7 +56,7 @@ def update_category(uid):
 def delete_category(uid):
     # ничего не возвращаем, 204 - NO CONTENT
     if uid not in storage:
-        abort(404)
+        abort(HTTPStatus.NOT_FOUND)
 
     storage.pop(uid)
     return {}, 204
--- backend/products.py
+++ backend/products.py
@@ -1,23 +1,23 @@
-from flask import Flask, jsonify, request, abort, Blueprint
 from http import HTTPStatus
 from uuid import uuid4
 
+from flask import Blueprint, abort, jsonify, request
+
 view = Blueprint('products', __name__)
 
-storage = [{
-        "id": uuid4().hex,
-        "tytle": "тренировки по волейболу",
-        "category": "что-то",
-    }, {
-        "id": uuid4().hex,
-        "tytle": "услуги массажа",
-        "category": "что-то"}]
+storage = [
+    {
+        'id': uuid4().hex,
+        'tytle': 'тренировки по волейболу',
+        'category': 'что-то',
+    },
+    {
+        'id': uuid4().hex,
+        'tytle': 'Услуги массажа',
+        'category': 'что-то',
+    },
+]
 
-
-
-# @view.route('/')
-# def server():
-#     return "Привет!"
 
 # Вывести список всех товаров
 @view.get('/')
@@ -31,43 +31,38 @@ def get_all_products():
 def get_product_by_id(uid):
     # возвращаем найденный объект
     for diction in storage:
-        if diction["id"] == uid:
+        if diction['id'] == uid:
             return jsonify(diction), 200
-    abort(404)
-    # task = filter(lambda t: t['id'] == task_id, tasks)
-    # if len(task) == 0:
-    #     abort(404)
-    # return jsonify({'task': task[0]})
-
-
+    abort(HTTPStatus.NOT_FOUND)
 # Добавить новый товар
+
+
 @view.post('/')
 def add_product():
-    # получить тело запроса можно с помощью модуля request payload = request.json()
+    # получить тело запроса можно с помощью request payload = request.json()
     product = {
         'id': uuid4().hex,
         'title': request.json['title'],
-        'category': request.json.get('category', "")
+        'category': request.json.get('category', ''),
     }
     storage.append(product)
     # должны вернуть созданный у нас объект
     return jsonify(product), 200
 
-# Обновить данные продукта
+
 @view.put('/<string:uid>')
 def update_product(uid):
     for diction in storage:
-        if diction["id"] == uid:
+        if diction['id'] == uid:
             diction['title'] = request.json.get('title', diction['title'])
             diction['category'] = request.json.get('category', diction['category'])
-    # должны вернуть измененный объект
-    return diction, 200
+        return diction, 200
 
-# Удалить показатель
+
 @view.delete('/<string:uid>')
 def delete_product(uid):
     for diction in storage:
-        if diction["id"] == uid:
+        if diction['id'] == uid:
             storage.remove(diction)
     # ничего не возвращаем, 204 - NO CONTENT
     return {}, 204
