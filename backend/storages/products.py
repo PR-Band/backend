d725462302b2b2f7e71e0ac3f7c05ece12df085e
Stas Petrov <petrovst@mail.ru>
2023-03-24 12:49:31 +0300

    feat: удалил ProductsStorage

--- backend/storages/products.py
+++ backend/storages/products.py
@@ -1,5 +1,4 @@
 from typing import Any
-from uuid import uuid4
 
 from sqlalchemy.exc import IntegrityError
 
@@ -35,35 +34,3 @@ class Pgstorage:
         product_delete = Product.query.get(uid)
         db_session.delete(product_delete)
         db_session.commit()
-
-
-class ProductsStorage:
-
-    def __init__(self, products) -> None:
-        self.storage = {product['id']: product for product in products}
-
-    def get_all(self):
-        return list(self.storage.values())
-
-    def get_by_id(self, uid):
-        return self.storage.get(uid)
-
-    def add(self, new_product):
-        new_uid = uuid4().hex
-        new_product['id'] = new_uid
-        self.storage[new_uid] = new_product
-        # должны вернуть созданный у нас объект
-        return new_product
-
-    def update(self, payload, uid):
-        old_product = self.storage.get(uid)
-        if not old_product:
-            return None
-        old_product.update(payload)
-        return payload
-
-    def delete(self, uid):
-        if uid not in self.storage:
-            return False
-        self.storage.pop(uid)
-        return True
