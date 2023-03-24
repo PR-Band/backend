ce035ef9a8c79db09a265dfdb0fdd2e4f8adfaf7
Stas Petrov <petrovst@mail.ru>
2023-03-24 11:37:11 +0300

    feat: добавил класс NotfoundError

--- backend/errors.py
+++ backend/errors.py
@@ -12,4 +12,7 @@ class ConflictError(AppError):
     def __init__(self, entity: str, method: str) -> None:
         super().__init__(f'Can not {method} {entity}', HTTPStatus.CONFLICT)
 
-# class NotfoundError(AppError):
+
+class NotfoundError(AppError):
+    def __init__(self, entity: str, method: str) -> None:
+        super().__init__(f' can not found {entity} in {method}', HTTPStatus.NOT_FOUND)
