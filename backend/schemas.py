9d5848fb7023ead9635a5a8233ab27680d1ee701
Andrey Rukin <rukin897@gmail.com>
2023-04-11 23:31:50 +0300

    feat: ручка get_products и связь product-user

--- backend/schemas.py
+++ backend/schemas.py
@@ -13,6 +13,7 @@ class Product(BaseModel):
     id: int
     title: str
     category_id: int
+    user_id: int
 
     class Config:
         orm_mode = True
--- backend/views/user.py
+++ backend/views/user.py
@@ -3,11 +3,13 @@ from http import HTTPStatus
 from flask import Blueprint, abort, jsonify, request
 
 from backend import schemas
+from backend.storages.products import Pgstorage
 from backend.storages.user import UserStorage
 
 view = Blueprint('user', __name__)
 
 storage = UserStorage()
+pgstorage = Pgstorage()
 
 
 @view.post('/')
@@ -19,3 +21,19 @@ def add_user():
     new_user = schemas.User(**payload)
     user = storage.add(new_user.tgid, new_user.username)
     return jsonify(schemas.User.from_orm(user).dict()), 200
+
+
+@view.get('/telegram/<int:uid>')
+def get_by_tgid(uid):
+    user = storage.get_by_tgid(tgid=uid)
+    return jsonify(schemas.User.from_orm(user).dict())
+
+
+@view.get('/<int:uid>/products/')
+def get_products(uid):
+    products = pgstorage.get_products(user_id=uid)
+    products_user = [
+        schemas.Product.from_orm(product).dict()
+        for product in products
+    ]
+    return jsonify(products_user)
